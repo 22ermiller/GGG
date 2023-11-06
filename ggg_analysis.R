b1c4afc379dee0b268da1ce11aa8831c5a9a17d0
22ermiller <22ermiller@gmail.com>
2023-11-06 13:46:34 -0700

    neural networks

--- ggg_analysis.R
+++ ggg_analysis.R
@@ -83,6 +83,64 @@ final_svm_preds <- tibble(id = test$id,
 vroom_write(final_svm_preds, "svm_predictions.csv", delim = ",")
 
 
+# Neural Networks ---------------------------------------------------------
+
+nn_recipe <- recipe(formula = type~., data = train) %>%
+  step_mutate_at(all_nominal_predictors(), fn = factor) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_range(all_numeric_predictors(), min=0, max=1) # scale to [0,1]
+
+nn_model <- mlp(hidden_units = tune(),
+                epochs = 50#,
+                #activation="relu"
+                ) %>%
+  set_engine("nnet") %>%
+  #set_engine("keras", verbose = 0) %>% # verbose  = 0 prints less in console
+  set_mode("classification")
+
+nn_wf <- workflow() %>%
+  add_recipe(nn_recipe) %>%
+  add_model(nn_model)
+
+nn_tuneGrid <- grid_regular(hidden_units(range=c(1, 60)),
+                            levels=40)
+
+# split data into folds
+folds <- vfold_cv(train, v = 10, repeats = 1)
+
+# run Cross validation
+CV_results <- nn_wf %>%
+  tune_grid(resamples = folds,
+            grid = nn_tuneGrid,
+            metrics = metric_set(roc_auc, accuracy))
+
+# find best parameters
+bestTune <- CV_results %>%
+  select_best("accuracy")
+
+# graph tuning parameters
+
+CV_results %>% collect_metrics() %>%
+  filter(.metric == "accuracy") %>%
+  ggplot(aes(x=hidden_units, y=mean)) +
+  geom_line()
+
+final_nn_workflow <- nn_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = train)
+
+# predict
+nn_preds <- predict(final_nn_workflow,
+                     new_data = test,
+                     type = "class")
+
+final_nn_preds <- tibble(id = test$id,
+                          type = nn_preds$.pred_class)
+
+vroom_write(final_nn_preds, "nn_predictions.csv", delim = ",")
+
+
+
 
 
 
